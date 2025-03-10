a7df3812978cbd898c2bb5bce2d7f29be2523435
Tapan0936T <tapanpatel2630@gmail.com>
2025-03-10 17:28:00 +0530

    just time pass create a colour chang ing site

---
+++ 01.js
@@ -0,0 +1,8 @@
+// Select the '.change_colour' div
+document.querySelector(".change_colour").addEventListener("click", function() {
+    // Generate a random color
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    
+    // Change the body's background color
+    document.body.style.backgroundColor = randomColor;
+});
